ef223093a8007c0a34ed47f8b1a5535e8b77ef12
caridade14 <carymorais143@gmail.com>
2026-07-09 12:10:59 +0200

    feat: expose natural light schema and LUT APIs

--- src/natural-light/index.js
+++ src/natural-light/index.js
@@ -2,8 +2,12 @@
 
 const solar = require("./solar-core");
 const atmosphere = require("./atmosphere-core");
+const inputSchema = require("./input-schema");
+const lut = require("./lut-core");
 
 module.exports = {
   ...solar,
-  ...atmosphere
+  ...atmosphere,
+  ...inputSchema,
+  ...lut
 };
