b0235289a70c28af6c5c4acaf9c98dcdeeb68d4a
caridade14 <carymorais143@gmail.com>
2026-07-09 14:25:27 +0200

    feat: route public LUT APIs through phase 4 pipeline

--- src/natural-light/index.js
+++ src/natural-light/index.js
@@ -7,7 +7,7 @@ const atmosphereAdvanced = require("./atmosphere-advanced");
 const multipleScattering = require("./multiple-scattering-core");
 const atmosphereMultiple = require("./atmosphere-multiple");
 const inputSchema = require("./input-schema");
-const lut = require("./lut-core");
+const lut = require("./lut-phase4");
 
 module.exports = {
   ...solar,
